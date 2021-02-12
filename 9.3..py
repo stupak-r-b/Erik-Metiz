0b9a9caa1f2524bb1851d70ae64f2325cbffcf53
Roman Stupak <stupak.roman.borisovich@gmail.com>
2021-02-12 19:50:33 +0200

    Solved the task 9.3.

---
+++ 9.3..py
@@ -0,0 +1,27 @@
+"""User class. This class takes data from user."""
+class User:
+    def __init__(self, first_name, last_name, age, phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.phone_number = phone_number
+
+    """Output user phone number"""
+    def describe_user(self):
+        print(f"Информация о пользователе: "
+              f"\n1.First name - {self.first_name}"
+              f"\n2.Last name {self.last_name}"
+              f"\n3.Age {self.age}"
+              f"\n4.Phone number {self.phone_number}\n")
+
+    def greet_user(self):
+        print(f"\nДобро пожаловать на наш сервис {self.first_name.title()} {self.last_name.title()}!\n")
+
+
+first_user = User(first_name="Roman", last_name="Stupak", age=27, phone_number=123456789)
+second_user = User(first_name="Alex", last_name="Tsapko", age=28, phone_number=987654321)
+
+first_user.describe_user()
+first_user.greet_user()
+second_user.describe_user()
+second_user.greet_user()
