1dba1b6d1eaba2f32208e58798d03bcc9fd669ac
Roman Stupak <stupak.roman.borisovich@gmail.com>
2021-02-14 23:51:41 +0200

    The task 9.10. is solved

---
+++ 9.10/my_restaurant.py
@@ -0,0 +1,8 @@
+from restaurant import Restaurant
+
+
+my_first_restaurant = Restaurant("food for dev", "american")
+my_first_restaurant.open_restaurant()
+my_first_restaurant.describe_restaurant()
+
+
